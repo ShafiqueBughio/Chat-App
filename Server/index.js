241c567bfc62dbd1a41d13d25e29910d53397c82
ShafiqueBughio <153521292+ShafiqueBughio@users.noreply.github.com>
2025-04-08 20:13:04 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -14,24 +14,11 @@ const {app,server} = require("./Sockets/index")
 app.use(cors(
     {
         // origin:process.env.FRONTEND_URL,
-       origin: ["https://chat-app-frontend-silk-phi.vercel.app"], // Allow both local and deployed frontend
+       origin: ["https://chat-app-frontend-silk-phi.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
     }
 ))
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://chat-app-frontend-silk-phi.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-    }
-    
-    next();
-});
 
 // app.get("/",(req,res)=>{
 //   res.send("Hello")
